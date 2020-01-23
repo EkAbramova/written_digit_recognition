0443cb436120728131fad299899fa31df9e4e6ce
ekaterina <abramova.katia@gmail.com>
2020-01-23 23:11:36 +0300

    Test commit

--- fnn_model.py
+++ fnn_model.py
@@ -30,8 +30,8 @@ class FNN():
                        'W2': weights['W2'],
                        'b2': weights['b2']}
 
-        def loss(self, X, y, reg=0.0):
-            """
+    def loss(self, X, y, reg=0.0):
+        """
             Compute the loss and gradients for a two layer fully connected neural
             network.
             Inputs:
@@ -44,34 +44,79 @@ class FNN():
             Returns:
             grads: Dictionary mapping parameter names to gradients of those parameters
               with respect to the loss function; has the same keys as self.params.
-            """
+        """
         # Unpack variables from the params dictionary
 
             # Unpack variables from the params dictionary
-            W1, b1 = self.params['W1'], self.params['b1']
-            W2, b2 = self.params['W2'], self.params['b2']
-            N, D = X.shape
+        W1, b1 = self.params['W1'], self.params['b1']
+        W2, b2 = self.params['W2'], self.params['b2']
+        N, D = X.shape
 
-            layer1 = X.dot(W1) + b1         # fully connected
-            X2 = np.maximum(0, layer1)      # ReLU
-            layer2 = X2.dot(W2) + b2        # fully connected
+        layer1 = X.dot(W1) + b1         # fully connected
+        X2 = np.maximum(0, layer1)      # ReLU
+        layer2 = X2.dot(W2) + b2        # fully connected
 
             # compute the class probabilities
-            exp_scores = np.exp(layer2)
-            probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)  # [N x K]
+        exp_scores = np.exp(layer2)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)  # [N x K]
 
             # Backward pass: compute gradients
-            grads = {}
-            probs[range(X.shape[0]), y] -= 1
-            dW2 = np.dot(layer1.T, probs)
-            dW2 /= X.shape[0]
-            dW2 += reg * W2
-            grads['W2'] = dW2
-            grads['b2'] = np.sum(probs, axis=0, keepdims=True) / X.shape[0]
+        grads = {}
+        probs[range(X.shape[0]), y] -= 1
+        dW2 = np.dot(layer1.T, probs)
+        dW2 /= X.shape[0]
+        dW2 += reg * W2
+        grads['W2'] = dW2
+        grads['b2'] = np.sum(probs, axis=0, keepdims=True) / X.shape[0]
 
-            delta = probs.dot(W2.T)
-            delta = delta * (layer1 > 0)
-            grads['W1'] = np.dot(X.T, delta) / X.shape[0] + reg * W1
-            grads['b1'] = np.sum(delta, axis=0, keepdims=True) / X.shape[0]
+        delta = probs.dot(W2.T)
+        delta = delta * (layer1 > 0)
+        grads['W1'] = np.dot(X.T, delta) / X.shape[0] + reg * W1
+        grads['b1'] = np.sum(delta, axis=0, keepdims=True) / X.shape[0]
 
-            return grads
+        return grads
+
+    def train(self, X, y, learning_rate=0.1 * (0.95 ** 24) / 32, reg=0.001, batch_size=24):
+        """
+            Train this neural network using stochastic gradient descent.
+            Inputs:
+            - X: A numpy array of shape (N, D) giving training data.
+            - y: A numpy array f shape (N,) giving training labels; y[i] = c means that
+              X[i] has label c, where 0 <= c < C.
+            - learning_rate: Scalar giving learning rate for optimization.
+            - reg: Scalar giving regularization strength.
+            - batch_size: Number of training examples to use per step.
+        """
+
+        num_train = X.shape[0]
+
+            # Compute loss and gradients using the current minibatch
+        grads = self.loss(X, y, reg=reg)
+
+        self.params['W1'] -= learning_rate * grads['W1']
+        self.params['b1'] -= learning_rate * grads['b1'][0]
+        self.params['W2'] -= learning_rate * grads['W2']
+        self.params['b2'] -= learning_rate * grads['b2'][0]
+
+    def predict(self, X):
+        """
+            Use the trained weights of this two-layer network to predict labels for
+            data points. For each data point we predict scores for each of the C
+            classes, and assign each data point to the class with the highest score.
+            Inputs:
+            - X: A numpy array of shape (N, D) giving N D-dimensional data points to
+              classify.
+            Returns:
+            - y_pred: A numpy array of shape (N,) giving predicted labels for each of
+              the elements of X. For all i, y_pred[i] = c means that X[i] is predicted
+              to have class c, where 0 <= c < C.
+        """
+        layer1 = X.dot(self.params['W1']) + self.params['b1']
+        layer1[layer1 < 0] = 0
+        layer2 = layer1.dot(self.params['W2']) + self.params['b2']
+
+        exp_scores = np.exp(layer2)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+        y_pred = np.argmax(probs, axis=1)
+
+        return y_pred
--- testing.py
+++ testing.py
@@ -1,7 +1,41 @@
 import tensorflow as tf
-
+import fnn_model
+import numpy as np
 
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train[:1000]
+y_train = y_train[:1000]
+x_test = x_test[:1000]
+y_test = y_test[:1000]
 
-print(x_train)
+input_size = 28 * 28 #* 3
+hidden_size = 100
+output_size = 10
+
+
+weights = {}
+weights['W1'] = 0.00001 * np.random.randn(input_size, hidden_size)
+weights['b1'] = np.zeros(hidden_size)
+weights['W2'] = 0.00001 * np.random.randn(hidden_size, output_size)
+weights['b2'] = np.zeros(output_size)
+
+
+x_train_r = x_train.reshape(1000, -1)
+x_train_r.shape
+
+x_test_r = x_test.reshape(1000, -1)
+
+model = fnn_model.FNN(weights)
+
+model.train(x_train_r, y_train)
+
+preds = model.predict(x_test_r)
+
+train_acc = (model.predict(x_test_r) == y_train).mean()
+print(train_acc)
+
+
+
+
+
