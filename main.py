dff88754355d5237070783f8ae57c484ea216588
Ekaterina Kasilina <abramova.katia@gmail.com>
2020-03-15 22:04:03 +0300

    make it better

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET", 'OPTIONS'])
 def index_page():
-	return render_template('templates/index.html')
+	return render_template('index.html')
 
 
 @app.route('/<name>')
