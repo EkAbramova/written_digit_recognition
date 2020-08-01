eca283138e1da028460d239d8a0412a85ce27bab
Ekaterina Kasilina <abramova.katia@gmail.com>
2020-08-01 21:40:12 +0300

    precompiled assets

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def index_page():
 	return render_template('index.html')
 
+@app.route("/models")
+def models():
+	return render_template('models.html')
+
 @app.route('/hook2', methods = ["GET", "POST", 'OPTIONS'])
 def main_predict():
 	"""
